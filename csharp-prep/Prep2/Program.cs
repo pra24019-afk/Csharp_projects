1d064c098e17dea511ea0690ad53e24b18c4b027
pra24019-afk <pra24019@byui.edu>
2026-01-09 15:16:27 -0700

    Update Grade Percentage.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        float grade = float.Parse(Console.ReadLine());
+        string letter = "";
+        bool passed = false;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            passed = true;
+        }else if (grade >= 80) {
+            letter = "B";
+            passed = true;
+        }else if (grade >= 70)
+        {
+            letter = "C";
+            passed = true;
+        }else if (grade >= 60)
+        {
+            letter = "D";
+        }else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (passed == true)
+        {
+            Console.Write("Well done you passed!");
+        }
+        else
+        {
+            Console.Write("You did not pass the class. Better luck next time.");
+        }
     }
 }
