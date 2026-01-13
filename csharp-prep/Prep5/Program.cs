3eb54506330bab053eb8cb8021bcc051c64ed545
pra24019-afk <pra24019@byui.edu>
2026-01-13 14:21:19 -0700

    Update Functions.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult();
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please Enter your Favorite Number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please Enter your Birth Year: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static double SquareNumber(int num)
+    {
+        double newNum = Math.Pow(num,2);
+        return newNum;
+    }
+
+    static void DisplayResult()
+    {
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        double square = SquareNumber(num);
+        int year;
+        PromptUserBirthYear(out year);
+        Console.WriteLine($"{name}, the square of your favorite number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2026-year} this year.");
+    }
+
+
 }
