e8f52e18806a28a199778136fe70e597c43e4c19
pra24019-afk <pra24019@byui.edu>
2026-01-13 11:25:43 -0700

    Update List Creator.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numArray = new List<int>();
+        int newNum;
+        float sum = 0;
+        int large = 0;
+        while (true)
+        {
+            Console.Write("Enter Number: ");
+            newNum = int.Parse(Console.ReadLine());
+            if (newNum == 0)
+            {
+                break;
+            }
+            else
+            {
+                numArray.Add(newNum);
+            }
+        }
+        foreach (int num in numArray)
+        {
+            sum += num;
+        }
+        float average = sum/numArray.Count;
+        foreach (int num in numArray)
+        {
+            if (num > large)
+            {
+                large = num;
+            }
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {large}");
     }
 }
