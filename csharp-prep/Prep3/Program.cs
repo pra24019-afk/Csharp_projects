8f5a60d8719afacdd2ac8e0b0a123e277d896a7a
pra24019-afk <pra24019@byui.edu>
2026-01-13 09:46:53 -0700

    Update Magic Number Guesser.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magicNum = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,100);
+        
+        bool guessed = false;
+        while (guessed == false)
+        {
+            Console.Write("What is your guess? ");
+            int num = int.Parse(Console.ReadLine());
+            if (num == magicNum)
+            {
+                guessed = true;
+                Console.WriteLine("You guessed it! Well done!");
+            }else if (num > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }else if (num < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
