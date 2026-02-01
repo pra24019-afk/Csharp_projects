495d8f1ba56b69855b8dcf8fae67443421ee8420
pra24019-afk <pra24019@byui.edu>
2026-01-31 21:00:39 -0700

    Update journal project.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal myJournal = new Journal();
+        myJournal._name = myJournal.AskName();
+
+        Entry entry1 = new Entry();
+        Entry entry2 = new Entry();
+        Entry entry3 = new Entry();
+
+        Console.WriteLine("Entry1");
+        entry1._dateOfEntry = entry1.AskDate();
+        entry1._entryDetails = entry1.AskDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Entry2");
+        entry2._dateOfEntry = entry2.AskDate();
+        entry2._entryDetails = entry2.AskDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Entry3");
+        entry3._dateOfEntry = entry3.AskDate();
+        entry3._entryDetails = entry3.AskDetails();
+        Console.WriteLine();
+
+        myJournal._entries.Add(entry1);
+        myJournal._entries.Add(entry2);
+        myJournal._entries.Add(entry3);
+        myJournal.DisplayJournal();
     }
 }
