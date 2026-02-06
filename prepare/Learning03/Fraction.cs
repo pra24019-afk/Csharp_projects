b2b0c3cfe97f2a4b5c986d98b48aaadd0a2ac388
pra24019-afk <pra24019@byui.edu>
2026-02-05 21:14:47 -0700

    Create Fraction.cs

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,51 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+    }
+    public string GetFractionString()
+    {
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+    public double GetDecimalValue()
+    {
+        double dec = (double)_top/_bottom;
+        return dec;
+    }
+}
