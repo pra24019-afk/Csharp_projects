8ec440cce1092b0b95ab8166193c89e5d1e98132
pra24019-afk <pra24019@byui.edu>
2026-02-05 21:14:19 -0700

    Update Fraction_Project.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Random random = new Random();
+        for (int i=0; i<25; i++){
+            int num = random.Next(0,100);
+            int den = random.Next(1,100);
+            fraction.SetTop(num);
+            fraction.SetBottom(den);
+            Console.WriteLine($"Fraction: {i}, string: {fraction.GetFractionString()}, Number: {fraction.GetDecimalValue()}");
+        }
     }
 }
