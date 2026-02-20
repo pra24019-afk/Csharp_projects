862f6a6b2b6b0f87c4b98926e39a052c2856fb68
pra24019-afk <pra24019@byui.edu>
2026-02-19 19:51:43 -0700

    Update Assignments Project.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment compito = new Assignment("David Crayola", "Calculus");
+        string summary = compito.GetSummary();
+
+        MathAssignment math = new MathAssignment("David Crayola", "Integration", "Section 3", "P- 1,2,3,4");
+        string homeworkList = math.GetHomeworkList();
+
+        WritingAssignment yuck = new WritingAssignment("David Crayola", "Creative Writing", "Rhetoric");
+        string writingInfo = yuck.GetWritingInformation();
+
+        Console.WriteLine("Assignments:");
+        Console.WriteLine(summary);
+        Console.WriteLine(homeworkList);
+        Console.WriteLine(writingInfo);
     }
 }
