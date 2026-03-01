bef5840365dc1750ad690f53658d8d0d2b93ddbd
pra24019-afk <pra24019@byui.edu>
2026-02-28 19:19:57 -0700

    Create Activity.cs

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Activity
+{
+    public Activity()
+    {
+        Console.WriteLine("This activity will help you to be more mindful.");
+        Console.Write("How long (in seconds) would you like the activity to last? ");
+    }
+}
