94a5ad115dc14d48263dd7793260cedc2ed40bcd
pra24019-afk <pra24019@byui.edu>
2026-02-28 19:19:33 -0700

    Update mindfulness Project.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,7 +3,58 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+    {   
+        bool cont = true;
+        while (cont == true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Menu");
+            Console.WriteLine("Options");
+            Console.WriteLine("Breathing:  Select 1");
+            Console.WriteLine("Reflection: Select 2");
+            Console.WriteLine("Listing:    Select 3");
+            Console.WriteLine("Journaling: Select 4");
+            Console.WriteLine("Quit:       Select 0");
+            Console.Write("Choose a mindfulness activity: ");
+
+            int option = int.Parse(Console.ReadLine());
+            Activity activity = new Activity();
+            int time = int.Parse(Console.ReadLine());
+            
+            
+            
+            
+
+            if (option == 1)
+            {
+
+                Breathing breathing = new Breathing(time);
+
+            }else if (option == 2)
+            {
+            
+                Reflection reflections = new Reflection(time);
+
+            }else if (option == 3)
+            {
+
+                Listing listing = new Listing(time);
+
+            }else if (option == 4)
+            {
+
+                Journaling journaling = new Journaling(time);
+
+            }else if (option == 0)
+            {
+                cont = false;
+                Console.WriteLine("Quitting");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+        }
+        
     }
 }
