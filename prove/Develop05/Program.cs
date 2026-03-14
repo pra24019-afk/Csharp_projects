5018b7511fcb3061ea66f22ef9b61c697ffe646d
pra24019-afk <pra24019@byui.edu>
2026-03-14 13:34:48 -0600

    Update Goal Project.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        //Console.WriteLine("Hello Develop05 World!");
+        GoalManager manager = new GoalManager();
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Record Event");
+            Console.WriteLine("4. Save Goals");
+            Console.WriteLine("5. Load Goals");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                manager.CreateGoal();
+            }else if (choice == "2")
+            {
+                manager.DisplayGoals();
+            }else if (choice == "3")
+            {
+                manager.RecordEvent();
+            }else if (choice == "4")
+            {
+                manager.SaveGoals();
+            }else if (choice == "5")
+            {
+                manager.LoadGoals();
+            }else if (choice == "6")
+            {
+                running = false;
+            }
+        }
     }
 }
