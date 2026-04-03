3348fc1ea85e0aac90bab452b2ba309009de77d2
pra24019-afk <pra24019@byui.edu>
2026-04-03 02:13:58 -0600

    Update Program.cs

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        FlashCardDeck deck = new FlashCardDeck();
         bool running = true;
         while (running == true)
         {
@@ -11,33 +13,82 @@ class Program
             Console.WriteLine("1. Add new multiple choice question");
             Console.WriteLine("2. Add new true / false question");
             Console.WriteLine("3. Add new short answer question");
-            Console.WriteLine("4. Examine existing questions");
+            Console.WriteLine("4. Review existing questions");
             Console.WriteLine("5. Begin quiz session");
             Console.WriteLine("6. Quit");
             int menuChoice = int.Parse(Console.ReadLine());
 
             if (menuChoice == 1)
             {
+                Console.WriteLine("Enter multiple choice question");
+                string prompt = Console.ReadLine();
+
+                List<string> choices = new List<string>();
                 
+                Console.Write("How many choices? ");
+                int numChoices = int.Parse(Console.ReadLine());
+
+                for (int i=0; i<numChoices; i++)
+                {
+                    Console.Write($"Enter choice {i+1}: ");
+                    choices.Add(Console.ReadLine());
+                }
+
+                Console.WriteLine("Enter the correct answer:");
+                string answer = Console.ReadLine();
+
+                MultipleChoiceQuestion q = new MultipleChoiceQuestion(prompt, choices, answer);
+                deck.AddQuestion(q);
+
+                Console.WriteLine("Question Added!");
+                Console.WriteLine();
+
             }else if (menuChoice == 2)
             {
-                
+                Console.WriteLine("Enter true/false question:");
+                string prompt = Console.ReadLine();
+
+                Console.WriteLine("Enter answer (true / false):");
+                bool answer = bool.Parse(Console.ReadLine());
+
+                TrueFalseQuestion q = new TrueFalseQuestion(prompt, answer);
+                deck.AddQuestion(q);
+
+                Console.WriteLine("Question Added!");
+                Console.WriteLine();
+
             }else if (menuChoice == 3)
             {
-                
+                Console.WriteLine("Enter short answer question:");
+                string prompt = Console.ReadLine();
+
+                Console.WriteLine("Enter the answer:");
+                string answer = Console.ReadLine();
+
+                ShortAnswerQuestion q = new ShortAnswerQuestion(prompt, answer);
+                deck.AddQuestion(q);
+
+                Console.WriteLine("Question Added!");
+                Console.WriteLine();
+
             }else if (menuChoice == 4)
             {
-                
+                foreach (Question q in deck.GetQuestions())
+                {
+                    q.Display();
+                    Console.WriteLine();
+                }
             }else if (menuChoice == 5)
             {
-                
+                QuizSession quiz = new QuizSession(deck);
+                quiz.Start();
             }else if (menuChoice == 6)
             {
                 running = false;
                 Console.WriteLine("Quitting...");
             }
 
-            //add flashcard deck with new questions to the flashcard deck with the correct and incorrect answers.
+            
         }
         
         
