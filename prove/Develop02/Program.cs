34a46a7e1168cde006290c09a43f2f4e382b7d32
pra24019-afk <pra24019@byui.edu>
2026-04-03 04:02:32 -0600

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,31 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Journal myJournal = new Journal();
-        myJournal._name = myJournal.AskName();
+        Console.WriteLine("Welcome to the Journal!");
+        Console.WriteLine("What is your name?");
+        string _name = Console.ReadLine();
+        Journal myJournal = new Journal(_name);
 
-        Entry entry1 = new Entry();
-        Entry entry2 = new Entry();
-        Entry entry3 = new Entry();
+        int _menuChoice;
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Write New Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save Journal");
+            Console.WriteLine("4. Load Journal");
+            Console.WriteLine("5. Quit");
+            _menuChoice = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Entry1");
-        entry1._dateOfEntry = entry1.AskDate();
-        entry1._entryDetails = entry1.AskDetails();
-        Console.WriteLine();
+            if (_menuChoice == 1)
+            {
+                Console.WriteLine("New Entry:");
+                Console.WriteLine("What is the date? (MMDDYYYY)");
+                int _dateOfEntry = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Entry2");
-        entry2._dateOfEntry = entry2.AskDate();
-        entry2._entryDetails = entry2.AskDetails();
-        Console.WriteLine();
+                Console.WriteLine("Entry Details:");
+                Console.Write("Optional entry topic: ");
+                Random rand = new Random();
+                int number = rand.Next(1, 5);
+                if (number == 1) {
+                    Console.WriteLine("What was the best part of my day?");
+                }else if (number == 2) {
+                    Console.WriteLine("What could I have done better today?");
+                }else if (number == 3) {
+                    Console.WriteLine("What made you laugh today?");
+                }else if (number == 4) {
+                    Console.WriteLine("How did you see the hand of the Lord today?");
+                }else if (number == 5) {
+                    Console.WriteLine("What made you happy today?");
+                }
+                string _entryDetails = Console.ReadLine();
 
-        Console.WriteLine("Entry3");
-        entry3._dateOfEntry = entry3.AskDate();
-        entry3._entryDetails = entry3.AskDetails();
-        Console.WriteLine();
+                Entry entry = new Entry(_dateOfEntry, _entryDetails, _name);
+                myJournal._entries.Add(entry);
 
-        myJournal._entries.Add(entry1);
-        myJournal._entries.Add(entry2);
-        myJournal._entries.Add(entry3);
-        myJournal.DisplayJournal();
+                Console.WriteLine("Entry Added!");
+                Console.WriteLine();
+
+            }else if (_menuChoice == 2)
+            {
+                myJournal.DisplayJournal();
+
+            }else if (_menuChoice == 3)
+            {
+                Console.WriteLine("What is the filename?");
+                string fileName = Console.ReadLine();
+                myJournal.SaveToFile(fileName);
+            }else if (_menuChoice == 4)
+            {
+                Console.WriteLine("What is the filename?");
+                string fileName = Console.ReadLine();
+                myJournal.LoadFromFile(fileName);
+            }else if (_menuChoice == 5)
+            {
+                running = false;
+                Console.WriteLine("Quitting...");
+            }
+            else {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine();
+            }
+        }
+
     }
 }
