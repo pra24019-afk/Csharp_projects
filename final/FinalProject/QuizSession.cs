33ab5d11157835cd35b331b3e0ace7c1f0d27349
pra24019-afk <pra24019@byui.edu>
2026-04-03 02:15:49 -0600

    Update QuizSession.cs

--- final/FinalProject/QuizSession.cs
+++ final/FinalProject/QuizSession.cs
@@ -9,4 +9,26 @@ public class QuizSession
         _deck = deck;
         _score = 0;
     }
+    public void Start()
+    {
+        foreach (Question q in _deck.GetQuestions())
+        {
+            q.Display();
+            string answer = Console.ReadLine();
+
+            if (q.CheckAnswer(answer))
+            {
+                Console.WriteLine("Correct!");
+                Console.WriteLine();
+                _score++;
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine($"Score: {_score}");
+        Console.WriteLine();
+    }
 }
