cde41313f33e7e7e22bb0ef05f539ea8a9092f4a
pra24019-afk <pra24019@byui.edu>
2026-04-03 02:34:36 -0600

    Update Program.cs

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Welcome to the study tool! What is your name? ");
+        string _name = Console.ReadLine();
+        User user = new User(_name);
 
         FlashCardDeck deck = new FlashCardDeck();
         bool running = true;
@@ -15,7 +18,8 @@ class Program
             Console.WriteLine("3. Add new short answer question");
             Console.WriteLine("4. Review existing questions");
             Console.WriteLine("5. Begin quiz session");
-            Console.WriteLine("6. Quit");
+            Console.WriteLine("6. Look at total score");
+            Console.WriteLine("7. Quit");
             int menuChoice = int.Parse(Console.ReadLine());
 
             if (menuChoice == 1)
@@ -81,16 +85,24 @@ class Program
             }else if (menuChoice == 5)
             {
                 QuizSession quiz = new QuizSession(deck);
-                quiz.Start();
+                user.AddScore(quiz.Start());
+
             }else if (menuChoice == 6)
+            {
+                Console.WriteLine($"{_name}'s total score from all quizzes is: {user.DisplayTotalScore()}");
+                Console.WriteLine();
+
+            }else if (menuChoice == 7)
             {
                 running = false;
                 Console.WriteLine("Quitting...");
             }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine();
+            }
 
-            
         }
-        
-        
     }
 }
