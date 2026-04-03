7622fffb0deea770c6037168a70dbd50abcef3a3
pra24019-afk <pra24019@byui.edu>
2026-04-03 02:14:32 -0600

    Update MultipleChoiceQuestion.cs

--- final/FinalProject/MultipleChoiceQuestion.cs
+++ final/FinalProject/MultipleChoiceQuestion.cs
@@ -7,5 +7,19 @@ public class MultipleChoiceQuestion: Question
     public MultipleChoiceQuestion(string prompt, List<string> choices, string answer): base(prompt)
     {
         _choices = choices;
+        _correctAnswer = answer;
+    }
+    public override bool CheckAnswer(string userAnswer)
+    {
+        return userAnswer.ToLower() == _correctAnswer.ToLower();
+    }
+    public override void Display()
+    {
+        Console.WriteLine(_prompt);
+        foreach (string choice in _choices)
+        {
+            Console.WriteLine(choice);
+        }
+        Console.WriteLine();
     }
 }
